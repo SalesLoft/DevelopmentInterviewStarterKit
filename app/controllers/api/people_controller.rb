11b7ab11aceb3c5895dc1eeb58aa9cc9cb3ee69c
svpease <sakatius@gmail.com>
2020-01-13 01:16:53 -0500

    Removed unneeded code

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -8,10 +8,7 @@ class Api::PeopleController < Api::BaseController
     if page < 1
       page = 1
     end
-    # Rails.logger.debug "My page is: #{page}."
-    # Rails.logger.debug  "!!! #{params[:page]}"
-    response = nil
-    # logger.debug "/api/people reached. :page == "
+    
     response = HTTParty.get("https://api.salesloft.com/v2/people.json?include_paging_counts=true&per_page=100&page=#{page}", {
       headers: {
         "Authorization" => "Bearer " + ENV["SALESLOFT_API_KEY"],
