cfa23b80a6d934f823346799c161bb130e769527
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-23 18:54:52 -0500

    comment about abstracting logic from the api request

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person
+  # abstracts from the api. this would make it easy to convert to using local persistance instead of api for every request.
   def self.all
     api = SalesloftApi.new
     api.people['data']
