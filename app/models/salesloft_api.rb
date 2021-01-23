c1fd10ec6b8b4591119f857844fc1e009048a742
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-23 18:14:02 -0500

    comment on pagination and scailing

--- app/models/salesloft_api.rb
+++ app/models/salesloft_api.rb
@@ -4,6 +4,9 @@ class SalesloftApi
     @authentication = authentication
   end
 
+  # The instructions don't specify that we must get the whole list of people, but just a list. In a real project i would get clarification on this, but for now I'll just grab the first page. 
+  # To get the whole list I might cycle through the pages using the "next_page" value and stopping when the value is null, reconstructing the results of each page into one array.
+  # With scailing in mind, I might use a chron job to regularly check the api using the updated_at parameter to get only the changes since last check. This would populate the local DB, and methods would work from that data.
   def people
     response = RestClient.get base_url + 'people', headers
     JSON.parse(response.body)
