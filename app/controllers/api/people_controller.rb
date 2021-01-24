aeffe0d19585521c14d6db1e4685059fe1b3a1fa
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-23 23:09:30 -0500

    render only needed attributes in from people_controller#index

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -2,6 +2,6 @@ class Api::PeopleController < Api::BaseController
   def index
     @people = Person.all
 
-    render json: @people.to_json, status: :ok
+    render json: @people.to_json(only: ["id", "display_name", "email_address", "title"]), status: :ok
   end
 end
