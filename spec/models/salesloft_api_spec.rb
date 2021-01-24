ef7747eff3a91adc27594defca7dad2fb0e9c1d7
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-24 12:09:02 -0500

    refactor salesloft_api_spec to use let

--- spec/models/salesloft_api_spec.rb
+++ spec/models/salesloft_api_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 describe SalesloftApi do
-  before(:each) do
-    @api = SalesloftApi.new
-  end
+  let(:api) { SalesloftApi.new }
 
   describe '#people' do
     it 'returns the body of the api request' do
       expected_result = JSON.parse(fake_salesloft_people)
-      expect(@api.people).to eq(expected_result)
+      expect(api.people).to eq(expected_result)
     end
   end
 end
