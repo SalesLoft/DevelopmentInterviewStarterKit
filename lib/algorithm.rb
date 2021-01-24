7b71029ced6480d219bfb7148dbafffe2b4558ba
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-24 16:36:16 -0500

    wip strings_with_tramsposed_characters

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -1,4 +1,17 @@
 class Algorithm
+  # ways to detect possible duplicates
+  # transposed letters: character count will be the same
+  # missed letter or accitental extra letter: character caount will be off by one, removing the extra letter would create a match
+  # return possible duplicates as an array of arrays (pairs or sets of possible duplicates)
+  def possible_duplicates(array)
+
+  end
+
+  # this is a start but it would catch any anagrams. I think we can do better
+  def strings_with_trasposed_characters(array)
+    array.group_by { |string| string.chars.sort }.reject { |chars, strings| strings.count < 2 }.values
+  end
+
   def self.count_and_sort_uniq_characters(obj)
     characters_count = count_uniq_characters(obj)
     sort_counted_characters(characters_count)
