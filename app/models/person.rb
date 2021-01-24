869a879cc4fe8b662d371ffc3d8b1d8416679dcb
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-24 14:06:45 -0500

    Person.email_character_count

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
+require 'algorithm'
+
 class Person
   attr_accessor :id, :display_name, :email_address, :title
 
@@ -8,6 +10,13 @@ class Person
     @title = attributes["title"]
   end
 
+  def self.email_character_count
+    emails = all.map(&:email_address)
+
+    Algorithm.count_and_sort_uniq_characters(emails)
+  end
+
+  # abstracts from the api. this would make it easy to convert to using local persistance instead of api for every request.
   def self.all
     api = SalesloftApi.new
 
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
+require 'algorithm'
 
 describe Person do
+  describe '.email_character_count' do
+    it "should return the count of email addresses for all people" do
+      emails = JSON.parse(fake_salesloft_people)['data'].map { |person| person["email_address"] }
+      expected_result = Algorithm.count_and_sort_uniq_characters(emails)
+      
+      expect(Person.email_character_count).to eq(expected_result)
+    end
+  end
+
   describe ".all" do
     let(:people) { Person.all }
 
     it 'returns a list of all people as person objects' do
-      expected_result = JSON.parse(fake_salesloft_people)['data']
       expected_count = JSON.parse(fake_salesloft_people)['data'].count
 
       expect(people.count).to eq(expected_count)
