bfe24af86c19331da3fc7bb66381590d72233582
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-23 23:00:25 -0500

    spec to test People#index format

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -6,5 +6,21 @@ describe Api::PeopleController do
       get :index
       expect(response).to be_successful
     end
+
+    it "should be formatted correctly" do
+      get :index
+      data = JSON.parse(response.body)
+      expected_count = JSON.parse(fake_salesloft_people)["data"].count
+
+      expect(data).to be_an Array
+      expect(data.count).to eq(expected_count)
+      data.each do |person|
+        expect(person).to be_a Hash
+        expect(person.keys).to include 'id'
+        expect(person.keys).to include 'display_name'
+        expect(person.keys).to include 'email_address'
+        expect(person.keys).to include 'title'
+      end
+    end
   end
 end
