92e2519537885d08f50a33d092fe66e82dd9a67e
Arthur Torres <arthur.torres@parkmobile.io>
2021-01-24 23:23:42 -0500

    Person.last_salesloft_update

--- app/models/person.rb
+++ app/models/person.rb
@@ -13,6 +13,14 @@ class Person < ApplicationRecord
     all.map(&:email_address)
   end
 
+  def self.last_salesloft_update
+    updated_ats = Person.all(api_only: false).pluck(:salesloft_updated_at)
+
+    return nil unless updated_ats.present?
+
+    updated_ats.sort.last
+  end
+
   def self.all(api_only: true)
     return super() unless api_only
 
